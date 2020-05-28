c056bfb6d0f9eec7251386f7acb484c71b1865f2
Harley Macedo <harley.ip@gmail.com>
2020-05-28 10:08:56 -0300

    Primeira tela com view engine

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const app = express()
+const handleBars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+//engine view
+app.engine('handlebars', handleBars({defaultLayout: 'main'}))
+
+//sets
+app.set('view engine', 'handlebars')
+
+//uses
+//app.use(handleBars())
+app.use(express.static(__dirname + '/public'))
+app.use(methodOverride('_method', {methods: ['GET', 'POST', 'PUT', 'DELETE']} ))
+app.use(methodOverride('X-HTTP-Method-Override'))
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
-    res.send('Teste inicial')
+    res.render('home')
 })
 
 app.listen(3000, () => {
