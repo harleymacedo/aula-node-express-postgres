16b2910d3a3bcb221b7c89f4a677c7eb7e5dc9eb
Harley Macedo <harley.ip@gmail.com>
2020-05-29 19:29:56 -0300

    Conexão com BD alterada

--- models/connection.js
+++ models/connection.js
@@ -1,11 +1,8 @@
 const Pool = require('pg')
+const url = 'postgres://mpheqkiiuzxupk:c3b806b8d0885a57f2df39911cf8f442c2422a5902f406e51f7debba1432b5d2@ec2-35-174-127-63.compute-1.amazonaws.com:5432/ddqjv2k7k5spa9'
 
 const con = new Pool({
-    host: 'ec2-35-174-127-63.compute-1.amazonaws.com',
-    database: 'ddqjv2k7k5spa9',
-    user: 'mpheqkiiuzxupk',
-    password: 'c3b806b8d0885a57f2df39911cf8f442c2422a5902f406e51f7debba1432b5d2',
-    port: '5432',
+    connectionString: url
 })
 
 module.exports = con
