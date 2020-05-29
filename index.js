faf339da8bc3bea8f7315460499369636de311b9
Harley Macedo <harley.ip@gmail.com>
2020-05-29 15:55:50 -0300

    Teste de resposta

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const handleBars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const cursosRouters = require('./routes/cursosRouters')
+//const cursosRouters = require('./routes/cursosRouters')
 
 //engine view
 app.engine('handlebars', handleBars({defaultLayout: 'main'}))
@@ -17,6 +17,10 @@ app.use(methodOverride('_method', {methods: ['GET', 'POST', 'PUT', 'DELETE']} ))
 app.use(methodOverride('X-HTTP-Method-Override'))
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.get('/cursos', cursosRouters.obterCursos)
+//app.get('/cursos', cursosRouters.obterCursos)
+app.get('/cursos', (req, res) => {
+    res.send('curso')
+})
 
-app.listen(process.env.PORT || 3000)
+var porta = process.env.PORT || 3000
+app.listen(porta)
